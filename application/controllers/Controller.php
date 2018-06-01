526f3f09df26c48db81f7543881353b6dc668ca3
Сергей Барабаш <sergey.barabash83@gmail.com>
2018-06-01 12:48:10 +0900

    layouts correction

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -12,39 +12,38 @@ use TestFramework\BaseController;
 
 class Controller extends BaseController {
 
-	/**
-	 * @var array
-	 */
-	private $itemsRowset;
-
 	public function indexPage(): void {
 		$productsRowset = (new ProductTableStub())->getAll();
 		$servicesRowset = (new ServiceTableStub())->getAll();
-		$this->itemsRowset = array_merge($productsRowset, $servicesRowset);
+		$itemsRowset    = array_merge($productsRowset, $servicesRowset);
 
-		$type     = $this->getParam('type');
-		$type     = $type ?? 'html';
+		$type = $this->getParam('type', 'html');
+
+		if ($type !== 'xml' && $type !== 'html') {
+			$type = 'html';
+		}
 		$fileType = ($type !== 'html') ? $type : 'phtml';
 
 		$this->view->render(
 			'index.' . $fileType,
-			'layout' . strtoupper($type) . '.phtml',
-			[ 'items' => $this->itemsRowset ]);
+			$type . '.phtml',
+			[ 'items' => $itemsRowset ]);
 	}
 
 	/**
-	 * @param string $paramName
+	 * @param string      $paramName
+	 * @param string|null $default
 	 * @return string|null
 	 */
-	private function getParam(string $paramName): ?string {
+	private function getParam(string $paramName, string $default = null): ?string {
 		if (isset($_GET[$paramName])) {
 			$type = trim($_GET[$paramName]);
 			$type = stripslashes($type);
 			$type = htmlspecialchars($type);
-			if ($type !== 'xml' && $type !== 'html') {
-				$type = 'html';
-			}
+		} else {
+			$type = $default;
 		}
-		return $type ?? null;
+
+		return $type;
 	}
 }
